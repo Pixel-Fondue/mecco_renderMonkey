f70e287de98cd4ef152eb376c855c0ab5d1e9854
adamohern <adam@ChocolateChip.local>
2015-12-05 20:11:54 -0500

    YAML template file and documentation

--- lxserv/renderMonkey_batchTemplate.py
+++ lxserv/renderMonkey_batchTemplate.py
@@ -2,7 +2,7 @@
 
 # By Adam O'Hern for Mechanical Color LLC
 
-import monkey, modo, lx, lxu, traceback, os, json, yaml
+import monkey, modo, lx, lxu, traceback, os, yaml
 
 CMD_NAME = 'renderMonkey.batchTemplate'
 
@@ -12,6 +12,9 @@ FRAMES = monkey.symbols.FRAMES
 DESTINATION = monkey.symbols.DESTINATION
 PATTERN = monkey.symbols.PATTERN
 GROUPS = monkey.symbols.GROUPS
+WIDTH = monkey.symbols.WIDTH
+HEIGHT = monkey.symbols.HEIGHT
+OUTPUTS = monkey.symbols.OUTPUTS
 
     
 class CMD(lxu.command.BasicCommand):
@@ -44,22 +47,6 @@ class CMD(lxu.command.BasicCommand):
                 modo.dialogs.customFile(
                     dtype='fileSave', 
                     title='Save Batch File Template',
-                    names=['json'],
-                    unames=['Batch File'],
-                    patterns=['*.json'],
-                    ext=['json']
-                )
-            )
-            
-            target = open(output_path,'w')
-            target.write(json.dumps(tree, sort_keys=False, indent=4, separators=(',', ': ')))
-            target.close()
-            
-            
-            output_path = os.path.normpath(
-                modo.dialogs.customFile(
-                    dtype='fileSave', 
-                    title='Save Batch File Template',
                     names=['yaml'],
                     unames=['Batch File (YAML)'],
                     patterns=['*.yaml'],
@@ -71,53 +58,69 @@ class CMD(lxu.command.BasicCommand):
             target.write(yaml.dump(tree, indent=4,width=999,default_flow_style = False).replace("\n-","\n\n-"))
             target.close()
             
-            readme = """Only 'path' is required. All other parameters are optional, see default values below.
 
-*path* - (required) Should contain a valid OS path to a MODO scene file. 
-
-*format* - (default: "*") Defaults to render output setting or, if none available, 16-bit EXR. Allows any of the following:
-"""
             
-            readme += "\n".join(["    %s: %s (*.%s)" % (i[0],i[1],i[2]) for i in monkey.util.get_imagesavers()]) + "\n\n"
-            readme += """*frames* - (default: *) A comma-delimited list of frame ranges in the format "start-end:step". Examples:
-    "*" : [As defined in file.]         << Use frame frange defined in 'start' and 'end' render channels.
-    "1" : [1]                           << Single frame
-    "1-5" : [1,2,3,4,5]                 << Frame sequence rendered in order.
-    "5-1" : [5,4,3,2,1]                 << Frames rendered in reverse order.
-    "0-10:2" : [0,2,4,6,8,10]           << Frames between zero and ten by twos.
-    "1-21:5" : [1,6,11,16,21]           << Frames between one and twenty-one by fives.
-    "1,1-5" : [1,2,3,4,5]               << Redundant frames will only be rendered once.
-    "(1 - 3),, 4-!@#5" : [1,2,3,4,5]    << Extra characters are stripped.
-
-*width* - (default: scene) Frame width in pixels. If a width is supplied but no height--or vise verse--the scene aspect ratio will be maintained.
-
-*height* - (default: scene) Frame height in pixels. If a width is supplied but no height--or vise verse--the scene aspect ratio will be maintained.
-
-*outputs* - (default: scene) List of render outputs to save, by name or id. If none are provided, all available render outputs will be rendered as per scene settings.
-
-*destination* - (default: "./frames/") Where to save the rendered frames. Examples:
-    "/already/perfectly/good/path/"     becomes     "/already/perfectly/good/path/"
-    "/path/with/file.xyz"               becomes     "/path/with/file.jpg" (extension ignored, will use format extension)
-    "/path/with/no_trailing_slash"      becomes     "/path/with/no_trailing_slash/"
-    "frames/"                           becomes     "/path/to/scene/file/frames/"
-    "./frames/"                         becomes     "/path/to/scene/file/frames/"
-    "~/fruit/loops/"                    becomes     "/path/to/user/home/fruit/loops/"
-    "pathalias:path/to/righteousness"   becomes     "/expanded/path/alias/path/to/righteousness/"
-    
-    Path aliases are useful for cross-platform LAN-based rendering, where drives cannot always be mapped to a common
-    system path between machines. These can be defined in MODO's preferences or programatically via kits or configs. See
-    MODO documentation.
-    
-*suffix* - (default: *) Sets the output pattern for file naming. Defaults to the scene file setting. For syntax, see
-    Output Patterns in MODO documentation.
-
-*passgroups* - (default: None) Pass groups to render for each frame. If a list of groups is provided, it will multiply
-    each successive group by the former. For example, ['group1','group2'] renders each pass of group2 for each pass of group1.
-    This is useful for pass groups containing orthogonal information, e.g. ['variations','views'] renders each view for each
-    variation.
-"""
+            readme = "\n\n# Syntax is YAML (yaml.org), interpreted with pyYAML.\n"
+            readme = "# A proper code editor is highly recommended for editing.\n"
+            readme = "# Brackets (http://brackets.io) is free, cross-platform, and has built-in YAML support.\n\n"
             
-            target = open(os.path.splitext(output_path)[0]+"_readme.md",'w')
+            readme += "# Each list item is a render task.\n"
+            readme += "# Only \"%s\" is required for a task to function. All other parameters are optional, see default values below.\n\n" % PATH
+
+            readme += "###\n\n"
+            
+            readme += "# \"%s\" - (required) Should contain a valid OS path to a MODO scene file.\n\n" % PATH
+
+            readme += "# \"%s\" - (default: *) Defaults to render output setting or, if none available, %s.\n" % (FORMAT,monkey.defaults.get('filetype'))
+            readme += "#    Allows any of the following:\n\n"
+            
+            readme += "#" + "\n#".join(["    %s: %s (*.%s)" % (i[0],i[1],i[2]) for i in monkey.util.get_imagesavers()]) + "\n\n"
+            
+            readme += "# \"%s\" - (default: *) A comma-delimited list of frame ranges in the format 'start-end:step'.\n" % FRAMES
+            readme += "#     Spaces and special characters are ignored, and redundant frames are only rendered once.\n"
+            readme += "#     Examples:\n\n"
+            
+            readme += "#    '*' -> Start/end frames defined in scene file.\n"
+            rr = ['1','1-5','5-1','0-10:2','1-21:5','1,1-5','(1 - 3),, 4-!@#5']
+            readme += "#" + "\n#".join(["    '%s' -> %s" % (i,str(monkey.util.range_from_string(i))) for i in rr]) + "\n\n"
+
+            readme += "# \"%s\" - (default: scene) Frame width in pixels.\n" % WIDTH
+            readme += "#     If a width is supplied but no height--or vise verse--the scene aspect ratio will be maintained.\n\n"
+
+            readme += "# \"%s\" - (default: scene) Frame height in pixels.\n" % HEIGHT
+            readme += "#     If a width is supplied but no height--or vise verse--the scene aspect ratio will be maintained.\n\n"
+
+            readme += "# \"%s\" - (default: scene) List of render outputs to save, by name or id.\n" % OUTPUTS
+            readme += "#     If none are provided, all available render outputs will be rendered as per scene settings.\n\n"
+
+            readme += "# \"%s\" - (default: %s) Where to save the rendered frames.\n" % (DESTINATION,monkey.defaults.get('destination'))
+            readme += "#    NOTE: Parsing is rather primitive. If the string begins with \"~\", it assumes you're parsing a user folder.\n"
+            readme += "#    If it starts with \".\" or lacks a leading slash, it assumes a relative path from the current scene.\n"
+            readme += "#    If it contains a \":\" anywhere at all, it assumes a MODO path alias. (Search for 'path alias' in MODO docs.)\n"
+            readme += "#    Using a file extension (e.g. 'filename.xyz') designates a file name, but the extension itself will be replaced as appropriate.\n"
+            readme += "#    Examples:\n\n"
+            
+            readme += "#    'frames/' -> /path/to/scene/file/frames\n"
+            readme += "#    '.frames/' -> /path/to/scene/file/frames\n"
+            readme += "#    '/path/with/filename.xyz' -> /path/with/file.jpg\n",
+            rr = [
+                "/already/perfectly/good/path/",
+                "/path/with/no_trailing_slash",
+                "~/fruit/loops/",
+                "kit_mecco_renderMonkey:path/to/righteousness"
+            ]
+            readme += "#" + "\n#".join(["    '%s' -> %s" % (i,str(monkey.util.expand_path(i))) for i in rr]) + "\n\n"
+    
+            readme += "# \"%s\" - (default: *) Sets the output pattern for file naming. Defaults to the scene file setting.\n" % PATTERN
+            readme += "#     For syntax, search for 'Output Pattern' in MODO docs and click the 'Render Item: Frame' link.\n\n"
+
+            readme += "# \"%s\" - (default: None) Pass groups to render for each frame.\n" % GROUPS
+            readme += "#     If a list of groups is provided, it will multiply each successive group by the former.\n"
+            readme += "#     For example, ['group1','group2'] renders each pass of group2 for each pass of group1.\n"
+            readme += "#     This is useful for pass groups containing orthogonal information,\n"
+            readme += "#     e.g. ['variations','views'] renders each 'view' pass for each 'variation' pass.\n"
+            
+            target = open(output_path,'a')
             target.write(readme)
             target.close()
                 
